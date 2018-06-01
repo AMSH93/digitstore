aa0be1a2cd2ef65e44847988e27edda60c259d91
Lewis Gyson <lewisgyson@Lewiss-MacBook-Pro.local>
2018-06-01 18:09:52 +1200

    user interface understands quit

--- view_console.c
+++ view_console.c
@@ -1,11 +1,25 @@
 #include "view_console.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 char inputBuffer[1024];
 
 command *getCommand(){
-	fgets((char *)&inputBuffer, 1024, stdin);
+	while(TRUE) {
+		fgets((char *)&inputBuffer, 1024, stdin);
 
-	printf("\n\n%s\n", inputBuffer);
-}
+		char cmdName[1024];
+		int digit;
+		char value[1024]; 
+
+		sscanf(inputBuffer, "%s %d %s", (char*)&cmdName, &digit, (char*)&value);  
+
+		if(strcmp("QUIT",cmdName)==0) {
+			printf("Ok QUIT\n");
+			return NULL;	
+		}
+		printf("UNKNOWN COMMAND\n");
+		printf(">");
+	}
+}	
